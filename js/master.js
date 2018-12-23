9730d04c708c2cf715cdd6d0aebab799d0cd0e81
“adminOneZero” <>
2018-12-23 15:13:17 +0200

    create doctors page

--- js/master.js
+++ js/master.js
@@ -1,13 +1,50 @@
-$("#clinics").iziModal({
-  title: ' جهه جديده ',
-  subtitle: ' اضف جهه جديده او عياده جديده للنظام ',
-  transitionOutOverlay: 'fadeOut',
-  background: '#34495e',
-  loop: false,
-  radius: 3,
-  borderBottom: true,
-});
+$(document).ready(function(){
+
+if ($('.mod').attr('data-page-name') == 'doctors') {
+  $("#page").iziModal({
+    title: ' اضافه طبيب ',
+    subtitle: ' اضافه اطباء جدد ',
+    headerColor: 'rgb(0,0,10,0.7)',
+    background: 'rgb(30,50,70,0.8)',
+    transitionOutOverlay: 'fadeOut',
+    loop: false,
+    radius: 3,
+    borderBottom: true,
+  });
+}
+
+if ($('.mod').attr('data-page-name') == 'clinics') {
+  $("#page").iziModal({
+    title: ' اضافه عيادة  ',
+    subtitle: ' اضف عيادة جديده ',
+    headerColor: 'rgb(0,0,10,0.7)',
+    background: 'rgb(30,50,70,0.8)',
+    transitionOutOverlay: 'fadeOut',
+    background: '#34495e',
+    loop: false,
+    radius: 3,
+    borderBottom: true,
+  });
+}
+
+
 $(document).on('click', '.trigger', function (event) {
-    event.preventDefault();
-    $('#clinics').iziModal('open');
+  event.preventDefault();
+
+  if (this.dataset.modal == 'doctors') {
+
+    $('.mod').iziModal('open');
+
+  }
+  if (this.dataset.modal == 'clinics') {
+
+    $('.mod').iziModal('open');
+
+  }
+});
+
+
+
+
+
 });
